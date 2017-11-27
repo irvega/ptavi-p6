45aa5c302a332dcac560ec627d465c68db81b2e2
Irene Vega Lago <ivega@gamma07.aulas.gsyc.urjc.es>
2017-11-27 15:30:51 +0100

    pep8

--- client.py
+++ client.py
@@ -13,22 +13,22 @@ if len(sys.argv) != 3:
 # Cliente UDP simple.
 # Dirección IP del servidor.
 LINE = sys.argv[2].split(':')[0]
-SERVER = sys.argv[2].split('@')[1].split(':')[0]
+IP = sys.argv[2].split('@')[1].split(':')[0]
 PORT = int(sys.argv[2].split('@')[1].split(':')[1])
 metodo = sys.argv[1]
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    my_socket.connect((SERVER, PORT))
-    
+    my_socket.connect((IP, PORT))
+
     print("Enviando: " + LINE)
     if metodo == 'INVITE':
-        my_socket.send(bytes('INVITE sip:' + LINE + ' SIP/2.0\r\n', 'utf-8') 
-                              + b'\r\n')
+        my_socket.send(bytes('INVITE sip:' + LINE + ' SIP/2.0\r\n', 'utf-8') +
+                       b'\r\n')
     if metodo == 'BYE':
-        my_socket.send(bytes('BYE sip:' + LINE + ' SIP/2.0\r\n', 'utf-8') 
-                              + b'\r\n')
+        my_socket.send(bytes('BYE sip:' + LINE + ' SIP/2.0\r\n', 'utf-8') +
+                       b'\r\n')
     data = my_socket.recv(1024)
 
     print('Recibido -- ', data.decode('utf-8'))
