40b71a625c6c4410cb6db6854c3e5dae161e0986
Irene Vega Lago <ivega@gamma18.aulas.gsyc.urjc.es>
2017-11-27 10:25:25 +0100

    Parte final

--- server.py
+++ server.py
@@ -3,7 +3,7 @@
 """
 Clase (y programa principal) para un servidor de eco en UDP simple
 """
-
+import os
 import socketserver
 import sys
 
@@ -11,7 +11,9 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
-
+    def error(self,line):
+        line_error = 
+    
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
         self.wfile.write(b"Hemos recibido tu peticion \r\n")
@@ -23,17 +25,22 @@ class EchoHandler(socketserver.DatagramRequestHandler):
             method = ((line.decode('utf-8')).split(' ')[0])
             if not line:
                 break
+            #if:
+                self.wfile.write(b'SIP/2.0 400 Bad Request')
             if method == lista[0]:
                 self.wfile.write(b'SIP/2.0 100 Trying \r\n')
                 self.wfile.write(b'SIP/2.0 180 Ringing \r\n')
                 self.wfile.write(b'SIP/2.0 200 OK  \r\n')
             elif method == lista[1]:
                 self.wfile.write(b'SIP/2.0 200 OK  \r\n')
+            elif method == lista[2]:
+                aEjecutar = './mp32rtp -i 127.0.0.1 -p 23032 < ' + sys.argv[3]
+                print("Vamos a ejecutar", aEjecutar)
+                os.system(aEjecutar)
             elif method not in lista:
                 self.wfile.write(b'SIP/2.0 405 Method Not Allowed')
             # Si no hay más líneas salimos del bucle infinito
-            else:
-                self.wfile.write(b'SIP/2.0 400 Bad Request')
+
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
